6d0da6d8ee40f748f289618748aff73cb6573bfe
AungKyaw321 <aung@neuroleapmail.com>
2023-08-01 18:20:35 -0800

    addedbreakers

--- dom.js
+++ dom.js
@@ -191,12 +191,12 @@ function advancedAdd() {
   const tagName = document.getElementById("elementTagName").value;
 
   if (selectedOption == "TextNode") {
-    newElement = document.createTextNode(elementContent || ` New Text Node`);
+    newElement = document.createTextNode(elementContent || `| New Text Node`);
   } else if (selectedOption == "Comment") {
-    newElement = document.createComment(elementContent || ` New Comment`);
+    newElement = document.createComment(elementContent || `| New Comment`);
   } else if (selectedOption == "Element") {
     newElement = document.createElement(tagName);
-    newElement.textContent = elementContent || ` New Element`;
+    newElement.textContent = elementContent || `| New Element`;
   }
   if (elementContent) {
     newElement.textContent += " - " + new Date().toLocaleString();
